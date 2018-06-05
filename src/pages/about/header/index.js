1343594be0986347e5c6d8606d580c487f354aa5
winer <392110917@qq.com>
2018-06-05 11:27:31 +0800

    first commit

--- src/pages/about/header/index.js
+++ src/pages/about/header/index.js
@@ -3,7 +3,11 @@ import React, {Component} from 'react'
 class Head extends Component {
   render () {
     return (
-      <div>header</div>
+      <section>
+        <div>winer</div>
+        <div>前端开发工程师</div>
+        <div>专注于构建从小型企业网站到丰富的交互式Web应用程序的所有内容。</div>
+      </section>
     )
   }
 }
