9f93b2be23a0b043a8903a6f43a951c9e33a44a6
winer <392110917@qq.com>
2018-06-06 00:03:42 +0800

    完成我的小DEMO页面实现

--- src/pages/about/myWorks/index.js
+++ src/pages/about/myWorks/index.js
@@ -10,11 +10,71 @@ class MyWorks extends Component {
     }
   }
   componentDidMount () {
-
+    this.setState({
+      works: [
+        {
+          id: 1,
+          img: 'http://jessdesigntan.com/images/styletribute.jpg',
+          link: 'https://www.baidu.com',
+          title: 'Styletrbute.com-1',
+          tags: ['html', 'js', 'css']
+        },
+        {
+          id: 2,
+          img: 'http://jessdesigntan.com/images/dearcarrie.jpg',
+          link: 'https://www.baidu.com',
+          title: 'Styletrbute.com-2',
+          tags: ['html', 'js', 'css']
+        },
+        {
+          id: 3,
+          img: 'http://jessdesigntan.com/images/nickel.jpg',
+          link: 'https://www.baidu.com',
+          title: 'Styletrbute.com-3',
+          tags: ['html', 'js']
+        },
+        {
+          id: 4,
+          img: 'http://jessdesigntan.com/images/sqrl.jpg',
+          link: 'https://www.baidu.com',
+          title: 'Styletrbute.com-4',
+          tags: ['html', 'js', 'css', 'react']
+        }
+      ]
+    })
   }
   render() {
+    const { works } = this.state
     return (
-      <section>123</section>
+      <section className="section">
+        <div className="section-left">
+          <div className="title no-color-title">
+            <h1>Works</h1>
+            <Icon type="schedule" />
+          </div>
+        </div>
+        <div className="section-right">
+          {
+            works.map((item, index) => {
+              return (
+                <div className="works-item" key={index}>
+                  <a target="blank" className="works-item-img" href={item.link}>
+                    <img src={item.img} />
+                  </a>
+                  <a target="blank" className="works-item-title" href={item.link}>
+                    <h2>{item.title}</h2>
+                  </a>
+                  <div className="tags">
+                    {
+                      item.tags.map((tag, index) => <div key={index} className="tag">{tag}</div>)
+                    }
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </section>
     )
   }
 }
